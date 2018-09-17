1970eb529a30c86e5d60b74128b1da7d14f7a78c
bmarchena <43344288+bmarchena@users.noreply.github.com>
2018-09-17 13:42:50 -0400

    Update MainActivity.java

--- java/bryanmarchena/scorecount/MainActivity.java
+++ java/bryanmarchena/scorecount/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int teamOne_int = 0, teamTwo_int = 0;
     private String win_team;
 
+    //declaration of strings for intent passing
     public static final String EXTRA_MESSAGE = "com.bryanmarchena.scorecount.EXTRA_MESSAGE";
     public static final String EXTRA_INT1 = "com.bryanmarchena.scorecount.EXTRA_INT1";
     public static final String EXTRA_INT2 = "com.bryanmarchena.scorecount.EXTRA_INT2";
@@ -27,17 +28,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //finding and casting views
         button_teamOne = (Button)findViewById(R.id.button_one);
         button_teamTwo = (Button)findViewById(R.id.button_two);
         textView_teamOne = (TextView)findViewById(R.id.teamOne_scoreDisplay);
         textView_teamTwo = (TextView)findViewById(R.id.teamTwo_scoreDisplay);
 
+        //initializing team scores
         textView_teamOne.setText("Score: " + String.valueOf(teamOne_int));
         textView_teamTwo.setText("Score: " + String.valueOf(teamTwo_int));
+        
+        //setting onclick listener
         button_teamOne.setOnClickListener((View.OnClickListener)this);
         button_teamTwo.setOnClickListener((View.OnClickListener)this);
     }
 
+    //overriding onClick method
     public void onClick(View view){
         if(button_teamOne.equals(view)){
             teamOne_int += 1;
@@ -56,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    //end of game method for starting WinnerActivity
     public void gameOver(){
         if (teamOne_int == Math.max(teamOne_int,teamTwo_int)){
             win_team = "TEAM 1";
@@ -64,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             win_team = "TEAM 2";
         }
 
+        //passing data from main to winner activities
         Intent intent = new Intent(this, WinnerActivity.class);
         intent.putExtra(EXTRA_MESSAGE, win_team);
         intent.putExtra(EXTRA_INT1,teamOne_int);
