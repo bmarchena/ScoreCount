e11d629d5160528de0b89b0e27c190f67af53ad2
Bryan <bryanmarchena@gmail.com>
2018-09-17 13:36:12 -0400

    Initial commit

---
+++ java/bryanmarchena/scorecount/WinnerActivity.java
@@ -0,0 +1,29 @@
+package bryanmarchena.scorecount;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class WinnerActivity extends AppCompatActivity {
+
+    private TextView textView_win;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winner);
+
+        textView_win = (TextView)findViewById(R.id.winner_text);
+
+        Intent intent = getIntent();
+        int teamOne_int = intent.getIntExtra(MainActivity.EXTRA_INT1, 0);
+        int teamTwo_int = intent.getIntExtra(MainActivity.EXTRA_INT2, 0);
+        String WinningTeam = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        int score_Spread = (Math.max(teamOne_int,teamTwo_int) - Math.min(teamOne_int,teamTwo_int));
+
+        textView_win.setText(WinningTeam + " won by: " + score_Spread);
+    }
+}
